72ca447cc7edcc16800a3a480fc3df222e4dce4a
mikururu <airantou1996413@gmail.com>
2023-06-06 01:49:37 +0800

    Update_腳色移動

---
+++ Assets/scripts/ControlSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+	[Header("移動速度")]
+	[Range(0,100)]
+	public float moveSpeed = 3.5f;
+
+	[Header("鋼體")]
+	public Rigidbody2D rig;
+	[Header("動畫控制器")]
+	public Animator ani;
+	[Header("跑步參數")]
+	public string parRun = "開關走路";
+	private void Awake()
+	{
+		
+	}
+	private void Start()
+	{
+		
+	}
+	private void Update()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		float h = Input.GetAxis("Horizontal");
+		rig.velocity = new Vector2(h, 0) * moveSpeed;
+
+		ani.SetBool(parRun,h != 0);
+	}
+}
+
---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+	public void StartGame() 
+	{
+		SceneManager.LoadScene("遊戲場景");
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
